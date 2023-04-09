e5e99bc88be1f27cfc3ffbef8d2aaee65030d1df
Victor Rodriguez Doncel <vrodriguez@fi.upm.es>
2023-04-09 18:39:50 +0200

    2

--- src/main/java/oeg/odrlevaluator/Main.java
+++ src/main/java/oeg/odrlevaluator/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * version0
+ * version2
  */
 @SpringBootApplication
 public class Main {
