c77b0f98d5c259fa582fd9ed95a78b30268472f6
Victor Rodriguez Doncel <vrodriguez@fi.upm.es>
2023-05-07 09:18:49 +0200

    First deployment tests

--- src/main/java/oeg/odrlevaluator/rest/StatusController.java
+++ src/main/java/oeg/odrlevaluator/rest/StatusController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import oeg.odrlevaluator.model.Status;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController {
     @ApiOperation(value = "Shows status information for this application.", notes = "Different types of status can be retrieved. <br>Default information: Application version, health of the ElasticSearch store<br>Log information: latest queries")
     @GetMapping(value = "", produces = "application/json")
+    @CrossOrigin(origins = "*")
     @ResponseBody
     public Status statusget(
     @ApiParam(name="value", value="Refines the status information",  example="") @RequestParam(defaultValue="", required = false) String value
